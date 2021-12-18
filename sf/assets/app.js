d437bfa95be606d49a4508fcaa1977d19ca8655e
Olivier <o.gonnet@gmail.com>
2021-12-18 19:58:41 +0100

    Symfony : PWA

--- sf/assets/app.js
+++ sf/assets/app.js
@@ -13,3 +13,14 @@ import './bootstrap';
 
 // Bootstrap
 require('bootstrap');
+
+// PWA Service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            // console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
--- sf/webpack.config.js
+++ sf/webpack.config.js
@@ -1,4 +1,6 @@
 const Encore = require('@symfony/webpack-encore');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -6,13 +8,88 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
+// define the pwa configuration
+Encore
+    .setOutputPath('public/')
+    .setPublicPath('/')
+
+    .addEntry('pwa', './assets/pwa.js')
+
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild(['public'], (options) => {
+        options.verbose = true;
+        options.root = __dirname;
+        options.exclude = ['images', '.htaccess', 'index.php'];
+    })
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    .addPlugin(
+        new WebpackPwaManifest({
+            filename: "manifest.webmanifest",
+            name: '0Bot',
+            short_name: '0Bot',
+            description: 'ccxt-quant-bot',
+            background_color: '#212529',
+            inject: true,
+            fingerprints: true,
+            theme_color: '#212529',
+            display: 'standalone',
+            start_url: '/?web-app',
+            ios: {
+                'apple-mobile-web-app-title': '0Bot',
+                'apple-mobile-web-app-status-bar-style': 'black'
+            },
+            crossorigin: null, //can be null, use-credentials or anonymous
+            icons: [
+                {
+                    src: 'public/images/bot.svg',
+                    sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+                    ios: true
+                },
+                {
+                    src: 'public/images/bot.svg',
+                    size: '1024x1024', // you can also use the specifications pattern
+                    ios: 'startup'
+                },
+                {
+                    src: 'public/images/bot.svg',
+                    size: '1024x1024',
+                    purpose: 'maskable'
+                }
+            ]
+        })
+    )
+
+    .addPlugin(
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true
+        })
+    )
+;
+
+// build the pwa configuration
+const pwaConfig = Encore.getWebpackConfig();
+
+// Set a unique name for the config (needed later!)
+pwaConfig.name = 'pwaConfig';
+
+// reset Encore to build the second config
+Encore.reset();
+
+// define the app configuration
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+    .setManifestKeyPrefix('build/')
 
     /*
      * ENTRY CONFIG
@@ -72,4 +149,10 @@ Encore
     //.autoProvidejQuery()
 ;
 
-module.exports = Encore.getWebpackConfig();
+// build the main configuration
+const appConfig = Encore.getWebpackConfig();
+
+// Set a unique name for the config (needed later!)
+appConfig.name = 'appConfig';
+
+module.exports = [pwaConfig, appConfig];
